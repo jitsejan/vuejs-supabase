ea627d722df9339c0b0880b6e57398dcfb6607ae
Jitse-Jan <code@jitsejan.com>
2021-04-13 03:00:03 +0100

    Add ordering

--- src/services/WordDataService.js
+++ src/services/WordDataService.js
@@ -2,7 +2,10 @@ import SUPABASE_CLIENT from "../config"
 
 class WordDataService {
     getAll() {
-        return SUPABASE_CLIENT.from("Words").select("id,word,definition")
+        return SUPABASE_CLIENT
+        .from("Words")
+        .select("id", "word", "definition")
+        .order('created_at', { ascending: false })
     }
 }
 
